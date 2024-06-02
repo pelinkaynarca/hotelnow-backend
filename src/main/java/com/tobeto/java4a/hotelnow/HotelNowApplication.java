abf0f29e839477b8feefd90d160c5dfbc799a762
omerfarukbala <omerfarukbala@gmail.com>
2024-06-02 23:02:46 +0300

    project initialization with required libraries

---
+++ src/main/java/com/tobeto/java4a/hotelnow/HotelNowApplication.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a.hotelnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobeto/java4a/hotelnow/HotelnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a.hotelnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
