9f1cdbad30fef8ce3e6da394943088d83830c503
omerfarukbala <omerfarukbala@gmail.com>
2024-06-07 00:12:13 +0300

    feat: add missing BookingHistory request & response dtos

---
+++ src/main/java/com/tobeto/java4a/hotelnow/services/dtos/requests/bookinghistories/AddBookingHistoryRequest.java
@@ -0,0 +1,28 @@
+package com.tobeto.java4a.hotelnow.services.dtos.requests.bookinghistories;
+
+import java.time.LocalDateTime;
+
+import com.tobeto.java4a.hotelnow.core.enums.BookingStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddBookingHistoryRequest {
+
+	private int userId;
+
+	private int bookingId;
+
+	private LocalDateTime editedAt;
+
+	private BookingStatus status;
+
+	private Integer cancellationReasonId;
+
+}
