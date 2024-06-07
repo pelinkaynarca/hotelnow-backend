335d294d4e577ac32d08bb1da7c39380a3209190
omerfarukbala <omerfarukbala@gmail.com>
2024-06-07 23:26:33 +0300

    feat: add mapper for Customer

---
+++ src/main/java/com/tobeto/java4a/hotelnow/services/mappers/CustomerMapper.java
@@ -0,0 +1,39 @@
+package com.tobeto.java4a.hotelnow.services.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.tobeto.java4a.hotelnow.entities.concretes.Customer;
+import com.tobeto.java4a.hotelnow.services.dtos.requests.customers.AddCustomerRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.requests.customers.UpdateCustomerRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.AddCustomerResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.ListCustomerResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.UpdateCustomerResponse;
+
+@Mapper
+public interface CustomerMapper {
+
+	CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+	
+	@Mapping(target = "countryName", source = "country.name")
+	@Mapping(target = "nationalityId", source = "country.id")
+	ListCustomerResponse listResponseFromCustomer(Customer customer);
+	
+	@Mapping(target = "role", ignore = true)
+	@Mapping(target = "staff", ignore = true)
+	@Mapping(target = "customer", ignore = true)
+	Customer customerFromAddRequest(AddCustomerRequest request);
+	
+	@Mapping(target = "nationalityId", source = "country.id")
+	AddCustomerResponse addResponseFromCustomer(Customer customer);
+	
+	@Mapping(target = "nationalityId", source = "country.id")
+	UpdateCustomerResponse updateResponseFromCustomer(Customer customer);
+	
+	@Mapping(target = "role", ignore = true)
+	@Mapping(target = "staff", ignore = true)
+	@Mapping(target = "customer", ignore = true)
+	Customer customerFromUpdateRequest(UpdateCustomerRequest request);
+
+}
