e6f4c46c979cc1369702eb4baf8ba65d85626440
omerfarukbala <omerfarukbala@gmail.com>
2024-06-08 11:31:18 +0300

    feat: add mapper for Staff

---
+++ src/main/java/com/tobeto/java4a/hotelnow/services/mappers/StaffMapper.java
@@ -0,0 +1,36 @@
+package com.tobeto.java4a.hotelnow.services.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.tobeto.java4a.hotelnow.entities.concretes.Staff;
+import com.tobeto.java4a.hotelnow.services.dtos.requests.staffs.AddStaffRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.staffs.AddStaffResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.staffs.ListStaffResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.staffs.UpdateStaffResponse;
+
+@Mapper
+public interface StaffMapper {
+
+	StaffMapper INSTANCE = Mappers.getMapper(StaffMapper.class);
+	
+	@Mapping(target = "hotelId", source = "hotel.id")
+	ListStaffResponse listResponseFromStaff(Staff staff);
+	
+	@Mapping(target = "hotelId", source = "hotel.id")
+	AddStaffResponse addResponseFromStaff(Staff staff);
+	
+	@Mapping(target = "hotelId", source = "hotel.id")
+	UpdateStaffResponse updateResponseFromStaff(Staff staff);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "role", ignore = true)
+	@Mapping(target = "staff", ignore = true)
+	@Mapping(target = "hotel", ignore = true)
+	@Mapping(target = "customer", ignore = true)
+	Staff staffFromAddRequest(AddStaffRequest addStaffRequest);
+
+}
