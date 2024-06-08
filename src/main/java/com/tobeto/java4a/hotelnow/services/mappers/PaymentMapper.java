d69ad9bdca61fdcc0a6c023ee73af776a25c6ed3
omerfarukbala <omerfarukbala@gmail.com>
2024-06-08 11:42:01 +0300

    feat: add mapper for Payment

---
+++ src/main/java/com/tobeto/java4a/hotelnow/services/mappers/PaymentMapper.java
@@ -0,0 +1,36 @@
+package com.tobeto.java4a.hotelnow.services.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.tobeto.java4a.hotelnow.entities.concretes.Payment;
+import com.tobeto.java4a.hotelnow.services.dtos.requests.payments.AddPaymentRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.requests.payments.UpdatePaymentRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.payments.AddPaymentResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.payments.ListPaymentResponse;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.payments.UpdatePaymentResponse;
+
+@Mapper
+public interface PaymentMapper {
+
+	PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
+
+	@Mapping(target = "bookingId", source = "booking.id")
+	ListPaymentResponse listResponseFromPayment(Payment payment);
+
+	@Mapping(target = "bookingId", source = "booking.id")
+	AddPaymentResponse addResponseFromPayment(Payment payment);
+
+	@Mapping(target = "bookingId", source = "booking.id")
+	UpdatePaymentResponse updateResponseFromPayment(Payment payment);
+
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "paymentDate", ignore = true)
+	@Mapping(target = "booking.id", source = "bookingId")
+	Payment paymentFromAddRequest(AddPaymentRequest addPaymentRequest);
+	
+	@Mapping(target = "booking.id", source = "bookingId")
+	Payment paymentFromUpdateRequest(UpdatePaymentRequest updatePaymentRequest);
+
+}
