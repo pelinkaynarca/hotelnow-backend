c5e85223e108e03532d499d1921e91ac563acfac
omerfarukbala <omerfarukbala@gmail.com>
2024-06-10 17:12:11 +0300

    refactor: standardize responses in globalexceptionhandler

--- src/main/java/com/tobeto/java4a/hotelnow/core/configurations/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/configurations/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.tobeto.java4a.hotelnow.core.configurations;
 
+import com.tobeto.java4a.hotelnow.controllers.BaseController;
 import com.tobeto.java4a.hotelnow.core.utils.exceptions.problemdetails.BusinessProblemDetails;
 import com.tobeto.java4a.hotelnow.core.utils.exceptions.problemdetails.ValidationProblemDetails;
 import com.tobeto.java4a.hotelnow.core.utils.exceptions.types.BusinessException;
+import com.tobeto.java4a.hotelnow.core.utils.messages.Messages;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.BaseResponse;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +17,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends BaseController {
 
-    @ExceptionHandler({BusinessException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public BusinessProblemDetails handleRuntimeException(BusinessException exception) {
-        return new BusinessProblemDetails(exception.getMessage());
-    }
+	@ExceptionHandler({ BusinessException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<BaseResponse<BusinessProblemDetails>> handleRuntimeException(BusinessException exception) {
+		return sendResponse(HttpStatus.BAD_REQUEST.value(), Messages.Error.CUSTOM_BAD_REQUEST,
+				new BusinessProblemDetails(exception.getMessage()));
+	}
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ValidationProblemDetails handleValidationException(MethodArgumentNotValidException exception) {
-        List<String> errorMessages = new ArrayList<>();
-        List<FieldError> errors = exception.getBindingResult().getFieldErrors();
-        for(FieldError error: errors) {
-            errorMessages.add(error.getDefaultMessage());
-        }
-        return new ValidationProblemDetails(errorMessages);
-    }
+	@ExceptionHandler({ MethodArgumentNotValidException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<BaseResponse<ValidationProblemDetails>> handleValidationException(
+			MethodArgumentNotValidException exception) {
+		List<String> errorMessages = new ArrayList<>();
+		List<FieldError> errors = exception.getBindingResult().getFieldErrors();
+		for (FieldError error : errors) {
+			errorMessages.add(error.getDefaultMessage());
+		}
+		return sendResponse(HttpStatus.BAD_REQUEST.value(), Messages.Error.CUSTOM_BAD_REQUEST,
+				new ValidationProblemDetails(errorMessages));
+	}
 }
