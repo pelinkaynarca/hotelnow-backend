7fb4fc1168e4729d9ab4bf6220c6e31130d6b6a0
omerfarukbala <omerfarukbala@gmail.com>
2024-06-10 17:30:22 +0300

    chore: remove unnecessary import

--- src/main/java/com/tobeto/java4a/hotelnow/core/configurations/fakeusers/FakeUser.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/configurations/fakeusers/FakeUser.java
@@ -3,7 +3,6 @@ package com.tobeto.java4a.hotelnow.core.configurations.fakeusers;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import com.tobeto.java4a.hotelnow.entities.concretes.Staff;
 import com.tobeto.java4a.hotelnow.entities.concretes.User;
 
 @ConfigurationProperties(prefix = "env.development.fakeuser")
