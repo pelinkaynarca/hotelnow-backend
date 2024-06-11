0cbe55d6387fd84f6f202655ba88f15e03980c32
omerfarukbala <omerfarukbala@gmail.com>
2024-06-11 18:29:40 +0300

    refactor: standardize response types of booking controller methods

--- src/main/java/com/tobeto/java4a/hotelnow/controllers/BookingsController.java
+++ src/main/java/com/tobeto/java4a/hotelnow/controllers/BookingsController.java
@@ -3,6 +3,7 @@ package com.tobeto.java4a.hotelnow.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,39 +12,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tobeto.java4a.hotelnow.core.utils.messages.Messages;
 import com.tobeto.java4a.hotelnow.services.abstracts.BookingService;
 import com.tobeto.java4a.hotelnow.services.dtos.requests.bookings.AddBookingRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.BaseResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.bookings.AddBookingResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.bookings.ListBookingResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/bookings")
-@AllArgsConstructor
-public class BookingsController {
+public class BookingsController extends BaseController {
 
 	private BookingService bookingService;
 
 	@GetMapping("/{id}")
-	public ListBookingResponse getById(@PathVariable int id) {
-		return bookingService.getResponseById(id);
+	public ResponseEntity<BaseResponse<ListBookingResponse>> getById(@PathVariable int id) {
+		return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY,
+				bookingService.getResponseById(id));
 	}
 
 	@GetMapping("/by-customer-id/{customerId}")
-	public List<ListBookingResponse> getByCustomerId(@PathVariable int customerId) {
-		return bookingService.getByCustomerId(customerId);
+	public ResponseEntity<BaseResponse<List<ListBookingResponse>>> getByCustomerId(@PathVariable int customerId) {
+		return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY,
+				bookingService.getByCustomerId(customerId));
 	}
 
 	@GetMapping("/by-hotel-id/{hotelId}")
-	public List<ListBookingResponse> getByHotelId(@PathVariable int hotelId) {
-		return bookingService.getByHotelId(hotelId);
+	public ResponseEntity<BaseResponse<List<ListBookingResponse>>> getByHotelId(@PathVariable int hotelId) {
+		return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY,
+				bookingService.getByHotelId(hotelId));
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public AddBookingResponse add(@RequestBody @Valid AddBookingRequest request) {
-		return bookingService.add(request);
+	public ResponseEntity<BaseResponse<AddBookingResponse>> add(@RequestBody @Valid AddBookingRequest request) {
+		return sendResponse(HttpStatus.CREATED.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY,
+				bookingService.add(request));
 	}
 
 }
