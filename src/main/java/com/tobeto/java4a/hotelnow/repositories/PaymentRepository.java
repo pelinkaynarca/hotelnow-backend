5ce3d32c5d349c8025176a45a76376453a2a52ec
omerfarukbala <omerfarukbala@gmail.com>
2024-06-12 12:02:57 +0300

    refactor: paymentrepository

--- src/main/java/com/tobeto/java4a/hotelnow/repositories/PaymentRepository.java
+++ src/main/java/com/tobeto/java4a/hotelnow/repositories/PaymentRepository.java
@@ -1,5 +1,7 @@
 package com.tobeto.java4a.hotelnow.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,6 @@ import com.tobeto.java4a.hotelnow.entities.concretes.Payment;
 public interface PaymentRepository extends JpaRepository<Payment, Integer>{
 	
 	@Query("select p from Payment p where p.booking.id = :bookingId limit 1")
-	Payment findByBookingId(@Param("bookingId") int bookingId);
+	Optional<Payment> findByBookingId(@Param("bookingId") int bookingId);
 
 }
