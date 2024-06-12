99819bdf6a4a9bcd172cf9d88089c845b4fc32ce
omerfarukbala <omerfarukbala@gmail.com>
2024-06-12 18:31:09 +0300

    refactor: standardize responses of bookinghistoriescontroller & roomtypescontroller

--- src/main/java/com/tobeto/java4a/hotelnow/controllers/RoomTypesController.java
+++ src/main/java/com/tobeto/java4a/hotelnow/controllers/RoomTypesController.java
@@ -21,53 +21,54 @@ import java.util.List;
 @AllArgsConstructor
 public class RoomTypesController extends BaseController {
 
-    private RoomTypeService roomTypeService;
+	private RoomTypeService roomTypeService;
 
-    @GetMapping("/get-all")
-    public ResponseEntity<BaseResponse<List<ListRoomTypeResponse>>> getAll() {
-        List<ListRoomTypeResponse> roomTypes = roomTypeService.getAll();
-        return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, roomTypes);
-    }
+	@GetMapping("/get-all")
+	public ResponseEntity<BaseResponse<List<ListRoomTypeResponse>>> getAll() {
+		List<ListRoomTypeResponse> roomTypes = roomTypeService.getAll();
+		return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, roomTypes);
+	}
 
-    @GetMapping("/hotel/{hotelId}")
-    public List<ListRoomTypeResponse> getByHotelId(@PathVariable("hotelId") int hotelId) {
-        return roomTypeService.getByHotelId(hotelId);
-    }
+	@GetMapping("/hotel/{hotelId}")
+	public List<ListRoomTypeResponse> getByHotelId(@PathVariable("hotelId") int hotelId) {
+		return roomTypeService.getByHotelId(hotelId);
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BaseResponse<ListRoomTypeResponse>> getById(@PathVariable int id) {
-        ListRoomTypeResponse roomType = roomTypeService.getById(id);
-        if (roomType == null) {
-            return sendResponse(HttpStatus.NOT_FOUND.value(), Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
-        } else {
-            return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, roomType);
-        }
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<BaseResponse<ListRoomTypeResponse>> getById(@PathVariable int id) {
+		ListRoomTypeResponse roomType = roomTypeService.getById(id);
+		if (roomType == null) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
+		} else {
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, roomType);
+		}
+	}
 
-    @PostMapping("/create-room-type")
-    public ResponseEntity<BaseResponse<AddRoomTypeResponse>> add(@RequestBody @Valid AddRoomTypeRequest request) {
-        AddRoomTypeResponse roomTypeResponse = roomTypeService.add(request);
-        return sendResponse(HttpStatus.CREATED.value(), Messages.Success.CUSTOM_CREATED_SUCCESSFULLY, roomTypeResponse);
-    }
+	@PostMapping("/create-room-type")
+	public ResponseEntity<BaseResponse<AddRoomTypeResponse>> add(@RequestBody @Valid AddRoomTypeRequest request) {
+		AddRoomTypeResponse roomTypeResponse = roomTypeService.add(request);
+		return sendResponse(HttpStatus.CREATED, Messages.Success.CUSTOM_CREATED_SUCCESSFULLY, roomTypeResponse);
+	}
 
-    @PutMapping("/update-room-type")
-    public ResponseEntity<BaseResponse<UpdateRoomTypeResponse>> update(@RequestBody @Valid UpdateRoomTypeRequest request) {
-        if (roomTypeService.getById(request.getId()) == null) {
-            return sendResponse(HttpStatus.BAD_REQUEST.value(), Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
-        }
+	@PutMapping("/update-room-type")
+	public ResponseEntity<BaseResponse<UpdateRoomTypeResponse>> update(
+			@RequestBody @Valid UpdateRoomTypeRequest request) {
+		if (roomTypeService.getById(request.getId()) == null) {
+			return sendResponse(HttpStatus.BAD_REQUEST, Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
+		}
 
-        UpdateRoomTypeResponse roomTypeResponse = roomTypeService.update(request);
-        return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_UPDATED_SUCCESSFULLY, roomTypeResponse);
-    }
+		UpdateRoomTypeResponse roomTypeResponse = roomTypeService.update(request);
+		return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_UPDATED_SUCCESSFULLY, roomTypeResponse);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<BaseResponse<String>> delete(@PathVariable int id) {
-        ListRoomTypeResponse deletedRoomType = roomTypeService.getById(id);
-        if (deletedRoomType == null) {
-            return sendResponse(HttpStatus.NOT_FOUND.value(), Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
-        } else {
-            roomTypeService.delete(id);
-            return sendResponse(HttpStatus.OK.value(), Messages.Success.CUSTOM_DELETED_SUCCESSFULLY, null);
-        }
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<BaseResponse<String>> delete(@PathVariable int id) {
+		ListRoomTypeResponse deletedRoomType = roomTypeService.getById(id);
+		if (deletedRoomType == null) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_ROOM_TYPE_NOT_FOUND, null);
+		} else {
+			roomTypeService.delete(id);
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_DELETED_SUCCESSFULLY, null);
+		}
+	}
 }
