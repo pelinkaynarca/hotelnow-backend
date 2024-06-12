59fb3ff82240d5ca4578c4217014fd896bb75e24
omerfarukbala <omerfarukbala@gmail.com>
2024-06-12 23:44:23 +0300

    refactor: standardize response types of reviews controller

--- src/main/java/com/tobeto/java4a/hotelnow/controllers/ReviewsController.java
+++ src/main/java/com/tobeto/java4a/hotelnow/controllers/ReviewsController.java
@@ -3,6 +3,7 @@ package com.tobeto.java4a.hotelnow.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tobeto.java4a.hotelnow.core.utils.messages.Messages;
 import com.tobeto.java4a.hotelnow.services.abstracts.ReviewService;
 import com.tobeto.java4a.hotelnow.services.dtos.requests.reviews.AddReviewRequest;
 import com.tobeto.java4a.hotelnow.services.dtos.requests.reviews.UpdateReviewRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.BaseResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.reviews.AddReviewResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.reviews.ListReviewResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.reviews.UpdateReviewResponse;
@@ -25,34 +28,51 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/reviews")
 @AllArgsConstructor
-public class ReviewsController {
-	
+public class ReviewsController extends BaseController {
+
 	private ReviewService reviewService;
-	
+
 	@GetMapping("/{id}")
-	public ListReviewResponse getById(@PathVariable int id) {
-		return reviewService.getById(id);
+	public ResponseEntity<BaseResponse<ListReviewResponse>> getById(@PathVariable int id) {
+		ListReviewResponse listReviewResponse = reviewService.getById(id);
+		if (listReviewResponse == null) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_REVIEW_NOT_FOUND, null);
+		} else {
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, listReviewResponse);
+		}
 	}
-	
+
 	@GetMapping("/by-booking-id/{bookingId}")
-	public ListReviewResponse getByBookingId(@PathVariable int bookingId) {
-		return reviewService.getByBookingId(bookingId);
+	public ResponseEntity<BaseResponse<ListReviewResponse>> getByBookingId(@PathVariable int bookingId) {
+		ListReviewResponse listReviewResponse = reviewService.getById(bookingId);
+		if (listReviewResponse == null) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_REVIEW_NOT_FOUND, null);
+		} else {
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, listReviewResponse);
+		}
 	}
-	
+
 	@GetMapping("/by-hotel-id/{hotelId}")
-	public List<ListReviewResponse> getByHotelId(@PathVariable int hotelId) {
-		return reviewService.getByHotelId(hotelId);
+	public ResponseEntity<BaseResponse<List<ListReviewResponse>>> getByHotelId(@PathVariable int hotelId) {
+		List<ListReviewResponse> listReviewResponses = reviewService.getByHotelId(hotelId);
+		if (listReviewResponses == null || listReviewResponses.size() == 0) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_REVIEW_NOT_FOUND, null);
+		} else {
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, listReviewResponses);
+		}
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public AddReviewResponse add(@RequestBody @Valid AddReviewRequest request) {
-		return reviewService.add(request);
+	public ResponseEntity<BaseResponse<AddReviewResponse>> add(@RequestBody @Valid AddReviewRequest request) {
+		AddReviewResponse addReviewResponse = reviewService.add(request);
+		return sendResponse(HttpStatus.CREATED, Messages.Success.CUSTOM_CREATED_SUCCESSFULLY, addReviewResponse);
 	}
-	
+
 	@PutMapping
-	public UpdateReviewResponse update(@RequestBody @Valid UpdateReviewRequest request) {
-		return reviewService.update(request);
+	public ResponseEntity<BaseResponse<UpdateReviewResponse>> update(@RequestBody @Valid UpdateReviewRequest request) {
+		UpdateReviewResponse updateReviewResponse = reviewService.update(request);
+		return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_UPDATED_SUCCESSFULLY, updateReviewResponse);
 	}
 
 }
--- src/main/java/com/tobeto/java4a/hotelnow/core/utils/messages/Messages.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/utils/messages/Messages.java
@@ -1,22 +1,22 @@
 package com.tobeto.java4a.hotelnow.core.utils.messages;
 
-
 public class Messages {
 
-    public static class Error {
-        public static final String CUSTOM_BAD_REQUEST = "Server Error. Please try again later!";
-        public static final String CUSTOM_ROOM_TYPE_NOT_FOUND = "The requested Room Type was not found.";
-        public static final String CUSTOM_STAFF_NOT_FOUND = "Staff(s) not found.";
-        public static final String AUTHORIZATION_VIOLATION = "You are not authorized to do this operation.";
-        public static final String CUSTOM_PAYMENT_NOT_FOUND = "Payment not found.";
-        public static final String CUSTOM_BOOKING_HISTORY_NOT_FOUND = "No booking history found.";
-        public static final String CUSTOM_CUSTOMER_NOT_FOUND = "Customer(s) not found.";
-    }
+	public static class Error {
+		public static final String CUSTOM_BAD_REQUEST = "Server Error. Please try again later!";
+		public static final String CUSTOM_ROOM_TYPE_NOT_FOUND = "The requested Room Type was not found.";
+		public static final String CUSTOM_STAFF_NOT_FOUND = "Staff(s) not found.";
+		public static final String AUTHORIZATION_VIOLATION = "You are not authorized to do this operation.";
+		public static final String CUSTOM_PAYMENT_NOT_FOUND = "Payment not found.";
+		public static final String CUSTOM_BOOKING_HISTORY_NOT_FOUND = "No booking history found.";
+		public static final String CUSTOM_CUSTOMER_NOT_FOUND = "Customer(s) not found.";
+		public static final String CUSTOM_REVIEW_NOT_FOUND = "Review(s) not found.";
+	}
 
-    public static class Success {
-        public static final String CUSTOM_LISTED_SUCCESSFULLY = "Successfully Listed";
-        public static final String CUSTOM_CREATED_SUCCESSFULLY = "Successfully Created";
-        public static final String CUSTOM_UPDATED_SUCCESSFULLY = "Successfully Updated";
-        public static final String CUSTOM_DELETED_SUCCESSFULLY = "Successfully Deleted";
-    }
+	public static class Success {
+		public static final String CUSTOM_LISTED_SUCCESSFULLY = "Successfully Listed";
+		public static final String CUSTOM_CREATED_SUCCESSFULLY = "Successfully Created";
+		public static final String CUSTOM_UPDATED_SUCCESSFULLY = "Successfully Updated";
+		public static final String CUSTOM_DELETED_SUCCESSFULLY = "Successfully Deleted";
+	}
 }
