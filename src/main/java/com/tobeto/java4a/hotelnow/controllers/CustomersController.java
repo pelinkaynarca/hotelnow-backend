4b152678c19f4d6721af1309c92a21eb212c6677
omerfarukbala <omerfarukbala@gmail.com>
2024-06-12 22:58:07 +0300

    refactor: standardize response types of customer controller methods

--- src/main/java/com/tobeto/java4a/hotelnow/controllers/CustomersController.java
+++ src/main/java/com/tobeto/java4a/hotelnow/controllers/CustomersController.java
@@ -1,6 +1,7 @@
 package com.tobeto.java4a.hotelnow.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tobeto.java4a.hotelnow.core.utils.messages.Messages;
 import com.tobeto.java4a.hotelnow.services.abstracts.CustomerService;
 import com.tobeto.java4a.hotelnow.services.dtos.requests.customers.AddCustomerRequest;
 import com.tobeto.java4a.hotelnow.services.dtos.requests.customers.UpdateCustomerRequest;
+import com.tobeto.java4a.hotelnow.services.dtos.responses.BaseResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.AddCustomerResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.ListCustomerResponse;
 import com.tobeto.java4a.hotelnow.services.dtos.responses.customers.UpdateCustomerResponse;
@@ -23,29 +26,38 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/customers")
 @AllArgsConstructor
-public class CustomersController {
-	
+public class CustomersController extends BaseController {
+
 	private CustomerService customerService;
-	
+
 	@GetMapping("/{id}")
-	public ListCustomerResponse getById(@PathVariable int id) {
-		return customerService.getResponseById(id);
+	public ResponseEntity<BaseResponse<ListCustomerResponse>> getById(@PathVariable int id) {
+		ListCustomerResponse listCustomerResponse = customerService.getResponseById(id);
+		if (listCustomerResponse == null) {
+			return sendResponse(HttpStatus.NOT_FOUND, Messages.Error.CUSTOM_CUSTOMER_NOT_FOUND, null);
+		} else {
+			return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_LISTED_SUCCESSFULLY, listCustomerResponse);
+		}
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public AddCustomerResponse add(@RequestBody @Valid AddCustomerRequest request) {
-		return customerService.add(request);
+	public ResponseEntity<BaseResponse<AddCustomerResponse>> add(@RequestBody @Valid AddCustomerRequest request) {
+		AddCustomerResponse addCustomerResponse = customerService.add(request);
+		return sendResponse(HttpStatus.CREATED, Messages.Success.CUSTOM_CREATED_SUCCESSFULLY, addCustomerResponse);
 	}
-	
+
 	@PutMapping
-	public UpdateCustomerResponse update(@RequestBody @Valid UpdateCustomerRequest request) {
-		return customerService.update(request);
+	public ResponseEntity<BaseResponse<UpdateCustomerResponse>> update(
+			@RequestBody @Valid UpdateCustomerRequest request) {
+		UpdateCustomerResponse updateCustomerResponse = customerService.update(request);
+		return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_UPDATED_SUCCESSFULLY, updateCustomerResponse);
 	}
-	
+
 	@DeleteMapping
-	public void delete(@PathVariable int id) {
-		customerService.delete(id);
+	public ResponseEntity<BaseResponse<String>> delete(@PathVariable int id) {
+		customerService.deleteById(id);
+		return sendResponse(HttpStatus.OK, Messages.Success.CUSTOM_DELETED_SUCCESSFULLY, null);
 	}
 
 }
--- src/main/java/com/tobeto/java4a/hotelnow/core/utils/messages/Messages.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/utils/messages/Messages.java
@@ -10,6 +10,7 @@ public class Messages {
         public static final String AUTHORIZATION_VIOLATION = "You are not authorized to do this operation.";
         public static final String CUSTOM_PAYMENT_NOT_FOUND = "Payment not found.";
         public static final String CUSTOM_BOOKING_HISTORY_NOT_FOUND = "No booking history found.";
+        public static final String CUSTOM_CUSTOMER_NOT_FOUND = "Customer(s) not found.";
     }
 
     public static class Success {
