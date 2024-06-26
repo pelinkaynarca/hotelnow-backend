0f502e47868aedac926f2b22dc5d8f42293bf91c
omerfarukbala <omerfarukbala@gmail.com>
2024-06-26 16:17:17 +0300

    refactor: remove unnecessary exception resolver

--- src/main/java/com/tobeto/java4a/hotelnow/core/filters/JwtFilter.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/filters/JwtFilter.java
@@ -4,37 +4,27 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-
 import com.tobeto.java4a.hotelnow.core.configurations.fakeusers.FakeUser;
 import com.tobeto.java4a.hotelnow.core.services.JwtService;
 import com.tobeto.java4a.hotelnow.entities.concretes.Role;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
 	private final JwtService jwtService;
 	private final FakeUser fakeUser;
-	private final HandlerExceptionResolver exceptionResolver;
-
-	public JwtFilter(JwtService jwtService, FakeUser fakeUser,
-			@Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver) {
-		this.jwtService = jwtService;
-		this.fakeUser = fakeUser;
-		this.exceptionResolver = handlerExceptionResolver;
-	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -51,11 +41,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
 			if (jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
 				String jwt = jwtHeader.substring(7);
-				try {
-					jwtService.validateToken(jwt);
-				} catch (JwtException e) {//to catch exceptions for invalid tokens(i.e. expired)
-					exceptionResolver.resolveException(request, response, null, e);
-				}
+				jwtService.validateToken(jwt);
 				username = jwtService.extractUsername(jwt);
 				roles = jwtService.extractRoles(jwt).stream().map((roleStr) -> Role.valueOf(roleStr))
 						.collect(Collectors.toList());
