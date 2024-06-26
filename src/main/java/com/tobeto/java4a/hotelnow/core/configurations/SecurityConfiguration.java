30b1c4dc0498aeebc253671bb0258e1078ad5868
omerfarukbala <omerfarukbala@gmail.com>
2024-06-26 13:30:54 +0300

    fix: cors issue

--- src/main/java/com/tobeto/java4a/hotelnow/core/configurations/SecurityConfiguration.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/configurations/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -61,7 +62,8 @@ public class SecurityConfiguration {
 				.httpBasic(AbstractHttpConfigurer::disable)
 				.exceptionHandling(e -> e.authenticationEntryPoint(authenticationEntryPoint))
 				.exceptionHandling(e -> e.accessDeniedHandler(accessDeniedHandler))
-				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+				.cors(Customizer.withDefaults())
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
