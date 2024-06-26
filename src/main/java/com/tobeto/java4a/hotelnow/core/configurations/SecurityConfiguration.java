88ec4e393efa2ce9f6d7d19a986eefb92a581734
omerfarukbala <omerfarukbala@gmail.com>
2024-06-26 14:59:32 +0300

    fix: cors issue

--- src/main/java/com/tobeto/java4a/hotelnow/core/configurations/SecurityConfiguration.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/configurations/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.tobeto.java4a.hotelnow.core.configurations;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import com.tobeto.java4a.hotelnow.core.filters.JwtFilter;
 import com.tobeto.java4a.hotelnow.services.abstracts.UserService;
 import lombok.RequiredArgsConstructor;
@@ -59,7 +53,7 @@ public class SecurityConfiguration {
 		http.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URLS).permitAll()
 						.anyRequest().authenticated())
-				.httpBasic(AbstractHttpConfigurer::disable)
+				.httpBasic(Customizer.withDefaults())
 				.exceptionHandling(e -> e.authenticationEntryPoint(authenticationEntryPoint))
 				.exceptionHandling(e -> e.accessDeniedHandler(accessDeniedHandler))
 				.cors(Customizer.withDefaults())
@@ -68,14 +62,14 @@ public class SecurityConfiguration {
 		return http.build();
 	}
 	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//	@Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200/*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
--- src/main/java/com/tobeto/java4a/hotelnow/core/configurations/WebConfiguration.java
+++ src/main/java/com/tobeto/java4a/hotelnow/core/configurations/WebConfiguration.java
@@ -3,10 +3,12 @@ package com.tobeto.java4a.hotelnow.core.configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
 	@Value("${spring.image.dir}")
@@ -14,8 +16,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
-		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE").allowedOrigins("http://localhost:4200").allowedHeaders("*");
+//		WebMvcConfigurer.super.addCorsMappings(registry);
 	}
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
